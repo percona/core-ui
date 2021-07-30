03201d00927708b7ef8598bdd172a79b294b230b
Alex Tymchuk <alexander.tymchuk@percona.com>
2021-07-30 04:41:46 +0300

    SAAS-598 ref TexareaInput tests

--- src/components/TextareaInput/TextareaInput.test.tsx
+++ src/components/TextareaInput/TextareaInput.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import { dataQa, FormWrapper } from 'shared';
+import { FormWrapper } from 'shared';
 import { TextareaInputField } from './TextareaInputField';
 
 describe('TextareaInputField::', () => {
