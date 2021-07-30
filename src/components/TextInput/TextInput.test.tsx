110fd493d0b576c69ef0efea7fdaa6dbe01a602b
Alex Tymchuk <alexander.tymchuk@percona.com>
2021-07-30 10:52:16 +0300

    SAAS-598 say ciao to enzyme

--- src/components/TextInput/TextInput.test.tsx
+++ src/components/TextInput/TextInput.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import { dataQa, FormWrapper } from 'shared';
+import { FormWrapper } from 'shared';
 import { TextInputField } from './TextInputField';
 
 describe('TextInputField::', () => {
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,15 +2,9 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-canvas-mock';
-import 'jest-enzyme';
-import { configure } from 'enzyme';
 import { configure as configureTestingLibrary } from '@testing-library/react';
-import * as Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-const adapter = Adapter as any;
-
-// eslint-disable-next-line new-cap
-configure({ adapter: new adapter.default() });
 configureTestingLibrary({ testIdAttribute: 'data-qa' });
