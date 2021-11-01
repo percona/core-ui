1f4d5a28a40b6a3dd03bd3d6584b68a6a1ee08f8
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2021-11-01 18:45:44 +0400

    PMM-9081-create-copyToClipboard-component: add component

---
+++ src/components/CopyToClipboard/CopyToClipboard.styles.ts
@@ -0,0 +1,27 @@
+import { GrafanaTheme } from '@grafana/data';
+import { css } from 'emotion';
+
+export const getStyles = (theme: GrafanaTheme) => {
+  const { spacing, colors, border } = theme;
+
+  return {
+    tooltip: css`
+      z-index: 1;
+    `,
+    tooltipText: css`
+      display: flex;
+      background-color: ${colors.bg2};
+      border-radius: ${border.radius.md};
+      box-shadow: 0 0 ${spacing.sm} 0 ${colors.dropdownShadow};
+      padding: ${spacing.sm};
+      color: ${colors.text};
+    `,
+    clipboardButton: css`
+      background-color: ${colors.bgBlue1};
+    `,
+    clipboardButtonContainer: css`
+      max-height: fit-content;
+      max-width: fit-content;
+    `,
+  };
+};
---
+++ src/components/CopyToClipboard/CopyToClipboard.tsx
@@ -0,0 +1,89 @@
+import React, {FC, useCallback, useRef, useState} from 'react';
+import {ClipboardButton, useStyles} from "@grafana/ui";
+import { ButtonProps } from "@grafana/ui/components/Button";
+import { usePopper } from "react-popper";
+import {Options as PopperOptions} from "@popperjs/core/lib/types";
+import { getStyles} from "./CopyToClipboard.styles";
+
+export interface ClipboardIconButtonProps extends ButtonProps {
+  textContainer: React.MutableRefObject<any>;
+}
+
+const popperConfig: Partial<PopperOptions> = {
+  placement: 'auto',
+  modifiers: [
+    {
+      name: 'offset',
+      enabled: true,
+      options: {
+        offset: [0, 10],
+      },
+    },
+  ],
+};
+
+export const CopyToClipboard: FC<ClipboardIconButtonProps> = (props) => {
+  const styles = useStyles(getStyles);
+
+  const { textContainer, ...rest } = props;
+
+  const [tooltipText, setTooltipText] = useState('');
+  const [visible, setVisible] = useState(false);
+
+  const copyToClipboard = useCallback(() => props.textContainer.current?.textContent? props.textContainer.current?.textContent : '', [props.textContainer]);
+
+  const popperRef = useRef(null);
+  const toggleRef = useRef(null);
+
+  const { styles: popperStyles, attributes: popperAttributes } = usePopper(
+    toggleRef.current,
+    popperRef.current,
+    popperConfig,
+  );
+
+  const showTooltip = () => {
+    setVisible(true);
+    setTimeout(() => setVisible(false), 2000);
+  }
+
+  const onClipboardCopy = () => {
+    setTooltipText('Text copied to clipboard');
+    showTooltip();
+  }
+
+  const onClipboardError = () => {
+    setTooltipText('Text not copied to clipboard');
+    showTooltip();
+  }
+
+  return (
+    <>
+      <div className={styles.clipboardButtonContainer} ref={toggleRef} >
+        <ClipboardButton
+          {...rest}
+          className={styles.clipboardButton}
+          onClipboardCopy={onClipboardCopy}
+          onClipboardError={onClipboardError}
+          getText={copyToClipboard}
+          icon="copy"/>
+      </div>
+      <div
+        ref={popperRef}
+        style={popperStyles.popper}
+        className={styles.tooltip}
+        {...popperAttributes.popper}
+        data-testid="tooltip-container"
+      >
+        {visible ? (
+          <div
+            className={styles.tooltipText}
+            style={popperStyles.offset}
+            data-testid="tooltip"
+          >
+            {tooltipText}
+          </div>
+        ) : null}
+      </div>
+    </>
+  )
+};
