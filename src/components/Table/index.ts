8cb819b2de40f1c14cd7e850ec8fff89b07d5b3a
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-11-02 18:08:25 +0100

    Fix import

--- src/components/Table/index.ts
+++ src/components/Table/index.ts
@@ -1,22 +1,2 @@
-// TODO: Remove this block after @types/react-table gets updated
-import { CSSProperties } from 'react';
-
-declare module 'react-table' {
-  interface Row {
-    isExpanded: boolean;
-    getToggleRowExpandedProps?: () => void;
-  }
-
-  interface HeaderGroup {
-    className?: string;
-    style?: CSSProperties;
-  }
-
-  interface ColumnInstance {
-    className?: string;
-    style?: CSSProperties;
-  }
-}
-
 export * from './Table';
 export * from './Table.types';
