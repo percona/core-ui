fd9e9612de8f915898b10fc20b21506974b6bf2f
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2022-07-21 15:49:52 +0400

    fix-max-length-validator: fix email validator

--- src/shared/validators/email.ts
+++ src/shared/validators/email.ts
@@ -1,4 +1,9 @@
 export const email = (value: string) => {
+  // No value would satisfy this validator by definition
+  if (value == null) {
+    return undefined;
+  }
+
   const emailRe = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
 
   return emailRe.test(value) ? undefined : 'Invalid email address';
