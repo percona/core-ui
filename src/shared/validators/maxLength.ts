717fc8e1a34aca2873795f65c42a940b344b19ff
Alex Tymchuk <alexander.tymchuk@gmail.com>
2022-07-21 14:05:51 +0300

    fix: maxLength validator

--- src/shared/validators/maxLength.ts
+++ src/shared/validators/maxLength.ts
@@ -1,6 +1,11 @@
 import { Validator } from './validator.types';
 
 export const maxLength = (length: number): Validator => (value: string) => {
+  // No value would satisfy this validator by definition
+  if (value == null) {
+    return undefined;
+  }
+  
   if (value.length <= length) {
     return undefined;
   }
