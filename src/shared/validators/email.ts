761bca856fd845fd4a1149122e44e2d6cc0fc5f3
Alex Tymchuk <alexander.tymchuk@gmail.com>
2022-07-23 17:55:35 +0300

    PMM-10353 fix eslint error

--- src/shared/validators/email.ts
+++ src/shared/validators/email.ts
@@ -1,5 +1,4 @@
-const emailRe =
-  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+const emailRe = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
 
 export const email = (value: string) => {
   // Checking for no value is the responsibility of the `required` validator
