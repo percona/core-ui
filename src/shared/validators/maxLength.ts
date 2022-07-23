55cbc59f15f15b26b63e157302978a6c18606672
Alex Tymchuk <alexander.tymchuk@percona.com>
2022-07-23 17:18:37 +0300

    PMM-10353 fix maxLength as well

--- src/shared/validators/maxLength.ts
+++ src/shared/validators/maxLength.ts
@@ -1,6 +1,11 @@
 import { Validator } from './validator.types';
 
 export const maxLength = (length: number): Validator => (value: string) => {
+  // This will avoid breaking the validador in cases when `value` is undefined
+  if (value == null) {
+    return undefined;
+  }
+
   if (value.length <= length) {
     return undefined;
   }
