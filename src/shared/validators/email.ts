508624e9ecb1f38b8893340c0c37e83b87e479aa
Alex Tymchuk <alexander.tymchuk@percona.com>
2022-07-23 17:34:38 +0300

    PMM-10353 move the assignment out of the function

--- src/shared/validators/email.ts
+++ src/shared/validators/email.ts
@@ -1,10 +1,11 @@
+const emailRe =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+
 export const email = (value: string) => {
   // No value would satisfy this validator by definition
   if (value == null) {
     return undefined;
   }
-
-  const emailRe = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
 
   return emailRe.test(value) ? undefined : 'Invalid email address';
 };
