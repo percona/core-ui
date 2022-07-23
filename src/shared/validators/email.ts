c106830b398f8f628a95f6da42860f53bbaadcff
Alex Tymchuk <alexander.tymchuk@percona.com>
2022-07-23 17:43:05 +0300

    PMM-10353 add a test for email

--- src/shared/validators/email.ts
+++ src/shared/validators/email.ts
@@ -2,7 +2,7 @@ const emailRe =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
 
 export const email = (value: string) => {
-  // No value would satisfy this validator by definition
+  // Checking for no value is the responsibility of the `required` validator
   if (value == null) {
     return undefined;
   }
