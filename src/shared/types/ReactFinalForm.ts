f3deed76285dc2dc219b1dafac62392b31c26bbc
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2022-07-29 12:05:29 +0400

    PMM-10340-Simpler-K8s-registration: adding type to onChange event

--- src/shared/types/ReactFinalForm.ts
+++ src/shared/types/ReactFinalForm.ts
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes, TextareaHTMLAttributes } from 'react';
+import { ChangeEvent, InputHTMLAttributes, TextareaHTMLAttributes } from 'react';
 
 type HtmlInputAttrs<T, IA = InputHTMLAttributes<T>> = {
   [P in keyof IA]?: IA[P];
@@ -13,4 +13,6 @@ export interface FieldInputAttrs extends Omit<HtmlInputAttrs<HTMLInputElement>, 
   multiple?: boolean;
 }
 
-export interface FieldTextareaAttrs extends Omit<HtmlTextareaAttrs<HTMLTextAreaElement>, 'name'> {}
+export interface FieldTextareaAttrs extends Omit<HtmlTextareaAttrs<HTMLTextAreaElement>, 'name'> {
+  onChange?: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+}
